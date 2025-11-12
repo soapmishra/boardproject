4a9c10536d97bdd7944af2653518343b7bdcb2e3
soapmishra <soapmishra@gmail.com>
2025-11-12 22:35:06 +0530

    chore: alias imported sqlite3 to 'connector'

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from objects import Account, Transaction
-import sqlite3
+import sqlite3 as connector
 
-def create_store(conn: sqlite3.Connection) -> None:
-    cur: sqlite3.Cursor = conn.cursor()
+def create_store(conn: connector.Connection) -> None:
+    cur: connector.Cursor = conn.cursor()
     tables: tuple[str, str] = ("accounts (account_number VARCAHR(20) PRIMARY KEY, name VARCHAR(100), balance DOUBLE DEFAULT 0.0, branch VARCHAR(20))",
                                                    "transactions (sender VARCHAR(20) NOT NULL, recipient VARCHAR(20) NOT NULL, value DOUBLE NOT NULL, FOREIGN KEY (sender) REFERENCES accounts(account_number), FOREIGN KEY (recipient) REFERENCES accounts(account_number))")
     for table in tables:
@@ -10,19 +10,22 @@ def create_store(conn: sqlite3.Connection) -> None:
             _ = cur.execute(f'CREATE TABLE {table}')
         except:
             pass
+    conn.commit()
+    cur.close()
 
-def load_accounts(conn: sqlite3.Connection) -> list[Account]:
-    cur: sqlite3.Cursor = conn.cursor()
+def load_accounts(conn: connector.Connection) -> list[Account]:
+    cur: connector.Cursor = conn.cursor()
     _ = cur.execute('SELECT * from accounts')
     accounts: list[Account] = []
     account: list[int | str | float]
     for account in cur.fetchall():
         (id, name, balance, branch) = (value for value in account)
         accounts.append(Account(id=id, name=name, balance=balance, branch=branch))
+    cur.close()
     return accounts
 
-def load_transactions(conn: sqlite3.Connection) -> list[Transaction]:
-    cur: sqlite3.Cursor = conn.cursor()
+def load_transactions(conn: connector.Connection) -> list[Transaction]:
+    cur: connector.Cursor = conn.cursor()
     _ = cur.execute('SELECT * from transactions')
     transactions: list[Transaction] = []
     transaction: list[int | float]
@@ -30,3 +33,10 @@ def load_transactions(conn: sqlite3.Connection) -> list[Transaction]:
         (sender, recipient, value) = (value for value in transaction)
         transactions.append(Transaction(sender=sender,recipient=recipient,value=value))
     return transactions
+
+def bankfunds(connection: connector.Connection) -> float:
+    transactions: list[Transaction] = load_transactions(connection)
+    balance: float = 0
+    for transaction in transactions:
+        balance += transaction.value
+    
