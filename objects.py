0a58d3979be7203465fd95c90affc1550c0945fb
soapmishra <soapmishra@gmail.com>
2025-11-12 16:50:36 +0530

    feat: implement contructor for class 'Account'

--- objects.py
+++ objects.py
@@ -1,5 +1,12 @@
+import random
+
 class Account:
-    pass
+    def __init__(self, name: str, branch: str, balance: float = 0, id: int = 0):
+        self.id: int = random.randint(1000, 9999) if id not in range(1000, 10000) else id
+        self.name: str = name
+        self.branch: str = branch
+        self.balance: float = balance
+
 
 class Transaction:
     pass
