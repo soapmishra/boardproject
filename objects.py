28568d47366d54803c9877f18c5ebd3af864dd88
soapmishra <soapmishra@gmail.com>
2025-11-14 20:56:20 +0530

    chore: created Administrator class for Administrator objects

--- objects.py
+++ objects.py
@@ -1,3 +1,6 @@
+import hashlib
+
+
 class Account:
 
     def __init__(self, id, name, balance, branch, type, deleted = 0):
@@ -38,4 +41,27 @@ class Transaction:
         return iter((self.sender, self.recipient, self.value))
 
     def __getitem__(self, item) -> int | float:
+        return tuple(self)[item]
+
+class Administrator:
+
+    def __init__(self, id, name, password):
+        self.id:int = int(id)
+        self.name:str = str(name)
+        self.__password:str = str(password)
+
+        # Generates a hash digest for the password
+        self.passwordhash = self.__password.encode('utf-8')
+        self.passwordhash = hashlib.sha256(self.passwordhash)
+
+    def __str__(self) -> str:
+        return f"{self.id},{self.name},{self.passwordhash}"
+
+    def __repr__(self) -> str:
+        return f"Administrator(ID: {self.id}, Name: {self.name}, Password: {self.__password})"
+    
+    def __iter__(self):
+        return iter((self.id, self.name, self.passwordhash))
+
+    def __getitem(self, item):
         return tuple(self)[item]
